539a7b4e535030a9214bea71da291afc08ec412c
XiaoHuanwise <wise_world@qq.com>
2018-12-07 14:18:51 +0800

    add a dir called Baiduhelper

---
+++ C#/Baiduhelper/Baiduhelper/Baiduhelper/BaiduHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Baiduhelper
+{
+    /*internal */class BaiduHelper
+    {
+        public static void Main(string[] args)
+        {
+            CBaiduhelper.BaiduTts.Tts("众里寻他千百度", "E:\\CJH\\voice.wav");
+        }
+    }
+}
---
+++ C#/Baiduhelper/Baiduhelper/CBaiduhelper/BaiduTts.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace CBaiduhelper
+{
+    public class BaiduTts
+    {
+        public static bool Tts(string TXT, string OPath)
+        {
+            // 设置APPID/AK/SK
+            var APP_ID = "14995725";
+            var API_KEY = "9j43q4PH3nf1uWhoaoN704FD";
+            var SECRET_KEY = "Rh7DAEiOymEZTaCci0pHYIKrhPUqkYfR";
+
+            var ttsClient = new Baidu.Aip.Speech.Tts(API_KEY, SECRET_KEY);
+            ttsClient.Timeout = 60000;  // 修改超时时间
+            
+            // 可选参数
+            var option = new Dictionary<string, object>()
+            {
+                {"spd", 5}, // 语速
+                {"vol", 7}, // 音量
+                {"per", 4}  // 发音人，4：情感度丫丫童声
+            };
+            var result = ttsClient.Synthesis(TXT, option);
+
+            if (result.ErrorCode == 0)  // 或 result.Success
+            {
+                File.WriteAllBytes(OPath, result.Data);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
